c78f9b0fddbdae93725edafd3677bd2c19a09de7
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2021-02-17 22:32:00 +0900

    rename Uncheck -> Unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,17 @@ use std::cmp::Ordering;
 use std::fmt::{Debug, Formatter};
 
 #[derive(PartialOrd, PartialEq)]
-pub struct UncheckOrd<T: PartialOrd + PartialEq>(pub T);
+pub struct UnwrapOrd<T: PartialOrd + PartialEq>(pub T);
 
-impl<T: PartialOrd + PartialEq> Eq for UncheckOrd<T> {}
+impl<T: PartialOrd + PartialEq> Eq for UnwrapOrd<T> {}
 
-impl<T: PartialOrd + PartialEq> Ord for UncheckOrd<T> {
+impl<T: PartialOrd + PartialEq> Ord for UnwrapOrd<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.partial_cmp(&other.0).unwrap()
     }
 }
 
-impl<T: PartialOrd + PartialEq + Debug> Debug for UncheckOrd<T> {
+impl<T: PartialOrd + PartialEq + Debug> Debug for UnwrapOrd<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "UncheckOrd({:?})", self.0)
     }
@@ -20,21 +20,21 @@ impl<T: PartialOrd + PartialEq + Debug> Debug for UncheckOrd<T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::UncheckOrd;
+    use crate::UnwrapOrd;
 
     #[test]
     fn it_works() {
         let v = [1.0, 3.0, 2.0];
-        let mut v: Vec<_> = v.iter().copied().map(|x| UncheckOrd(x)).collect();
+        let mut v: Vec<_> = v.iter().copied().map(|x| UnwrapOrd(x)).collect();
 
         v.sort();
 
         assert_eq!(
             v,
             vec![
-                UncheckOrd(1.0_f64),
-                UncheckOrd(2.0_f64),
-                UncheckOrd(3.0_f64)
+                UnwrapOrd(1.0_f64),
+                UnwrapOrd(2.0_f64),
+                UnwrapOrd(3.0_f64)
             ]
         );
     }
